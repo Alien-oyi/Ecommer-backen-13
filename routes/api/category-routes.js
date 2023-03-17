4f96ed0bf39e73e5a12afb92a07eb21d90b0f4fd
Alien-oyi <daijinmin78@gmail.com>
2023-03-16 20:22:29 -0400

    category post routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -46,8 +46,14 @@ router.get('/:id', (req, res) => {
   });
 })
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(201).json(categoryData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
